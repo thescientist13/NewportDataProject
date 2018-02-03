14a72e60472d4a52c9e1c64612023d263d4d9229
Edward Dupre <edwd42@gmail.com>
2018-02-03 01:41:00 +0000

    add leaf icons

--- Leaflet/script/geojson.js
+++ Leaflet/script/geojson.js
@@ -1,9 +1,22 @@
 //
+
 /*global L*/
+
+/*
+example .geojson file
+{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+    { "type": "Feature", "properties": { "layer": 0, "highway": "footway", "footway": "crossing" }, "geometry": { "type": "LineString", "coordinates": [ [ -122.034302600000018, 37.30423540000001 ], [ -122.0342822, 37.3040975 ] ] } },
+    { "type": "Feature", "properties": { "layer": 0, "highway": "footway", "footway": "crossing" }, "geometry": { "type": "LineString", "coordinates": [ [ -122.033809750159989, 37.303760214562843 ], [ -122.033748607028969, 37.303882324886331 ] ] } }
+] } 
+
+*/
 
 var places_geojson={
     "type": "FeatureCollection",
-    "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"}},
+    "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
     
     "features": [
         {"type": "Feature", "properties": {name: "marker1"}, "geometry": {"type": "Point", "coordinates": [41.728056, -71.475549] }},
@@ -17,3 +30,5 @@ L.geoJSON(places_geojson,{
             return L.marker(latlng);
         }
     }).addTo(mymap);
+    
+    
