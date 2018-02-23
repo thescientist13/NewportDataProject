c2c324cb0c0347af875c8c8bc0b09750be8b4d02
Edward Dupre <edwd42@gmail.com>
2018-02-23 14:21:00 +0000

    add bpac_survey_2014.geojson layer

--- Leaflet/js/script_blank.js
+++ Leaflet/js/script_blank.js
@@ -1,8 +1,10 @@
+// video tutorial https://youtu.be/fDfkjx-VhLI
 // js and Leaflet stuff
 
 /*global onMapClick*/
 /*global L*/
 /*global newport_hotels*/
+/*global bpac_survey_2014*/
 
 
 // center: [41.489805, -71.318002],
@@ -12,38 +14,123 @@ var map = L.map('map', {
     zoom: 15
 });
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 22,
-    // choose streets, satellite, or both   
-    id: 'mapbox.streets',
-    // id: 'mapbox.satellite',
-    // id: 'mapbox.streets-satellite',
-    accessToken: 'pk.eyJ1IjoiZWR3ZDQyIiwiYSI6ImNqY3oyMG5lbjFjZ3czNG5zdW0wcjc1cTMifQ.N_St7yHopIDrk4ln9aKtWA'
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18
 }).addTo(map);
 
-// add marker
-var teardrop = new L.Icon({iconUrl: 'marker-icon.png'});
+// add markers
+
+// blue marker for hotels
+var blueMarker = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.marker([51.5, -0.09], {icon: blueMarker}).addTo(map);
+
+// green marker for walking
+var greenMarker = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.marker([51.5, -0.09], {icon: greenMarker}).addTo(map);
+
+// red marker for Newport Crosswalk Safety Survey (2014) dataset
+// source https://github.com/NewportDataPortal/newport-crosswalk-survey-2014
+var redMarker = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.marker([51.5, -0.09], {icon: redMarker}).addTo(map);
+
+
 
 // add some data
 
-// function basementDweller(feature, layer){
-function onEachFeature(feature, layer){
+function newportHotels(feature, layer){
     layer.bindPopup("<strong>" + feature.properties.name + "</strong><br/>" +
                     feature.properties.street + "<br/>" +
                     feature.properties.city + ", " +
                     feature.properties.state
                     );
-    layer.setIcon(teardrop);
+    layer.setIcon(blueMarker);
 }
 
 
 L.geoJSON(newport_hotels, {
-    onEachFeature: onEachFeature
+    onEachFeature: newportHotels
 }).addTo(map);
 
-// L.geoJSON(newport_hotels,{
-//         pointToLayer: function (feature, latlng){
-//             return L.marker(latlng);
-//         }
-//     }).addTo(map);
+
+
+/*
+id: 1,
+Safe: "17",
+Unsafe: "114",
+"I don't know.": "112",
+score: "0.1297709923664122",
+n: "131",
+size: "14.30131004366812"
+feature.properties.id + "<br/>" +
+feature.properties.Safe + "<br/>" +
+feature.properties.Unsafe
+*/
+
+// function name similar to data file but must not be the same
+function bpacSurvey2014(feature, layer){
+    layer.bindPopup("<table id='t01'>" +
+                        "<tr>" +
+                            "<th>Key</th>" +
+                            "<th>Value</th>" + 
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>ID</td>" +
+                            "<td>" + feature.properties.id + "</td>" + 
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Safe</td>" +
+                            "<td>" + feature.properties.Safe + "</td>" + 
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Unsafe</td>" +
+                            "<td>" + feature.properties.Unsafe + "</td>" + 
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>I don't know</td>" +
+                            "<td>" + feature.properties.Idontknow + "</td>" + 
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Score</td>" +
+                            "<td>" + feature.properties.score + "</td>" + 
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>n</td>" +
+                            "<td>" + feature.properties.n + "</td>" + 
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Size</td>" +
+                            "<td>" + feature.properties.size + "</td>" + 
+                        "</tr>" +
+                    "</table>"
+                    );
+    layer.setIcon(redMarker);
+}
+
+L.geoJSON(bpac_survey_2014, {
+    onEachFeature: bpacSurvey2014
+}).addTo(map);
